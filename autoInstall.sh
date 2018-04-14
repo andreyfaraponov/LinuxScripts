6d6ae9b68d94674a9075cd3f66709e6d4ad3feeb
Andrey Faraponov <andrey.faraponov@gmail.com>
2018-04-14 12:01:16 +0300

    a

--- autoInstall.sh
+++ autoInstall.sh
@@ -16,6 +16,7 @@ apt-get update
 ###########################################
 ################## Apps ###################
 ###########################################
+apt-get install gconf-service libasound2 libatk1.0-0 libcairo2 libcups2 libfontconfig1 libgdk-pixbuf2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libxss1 fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils
 apt-get install $PACKAGES -y
 
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
