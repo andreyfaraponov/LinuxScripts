f04424d42240569fd7074abe001ab05424fda9c9
Andrey Faraponov <andrey.faraponov@gmail.com>
2018-04-14 11:55:39 +0300

    added node and angular

--- autoInstall.sh
+++ autoInstall.sh
@@ -4,16 +4,22 @@
 ###########################################
 HOSTNAME='fire'
 USERNAME='omega'
-PACKAGES='vim git build-essential chromium-browser libssl-dev apt-file'
+PACKAGES='vim git build-essential curl chromium-browser libssl-dev apt-file'
 
 ###########################################
 ################# Updates #################
 ###########################################
-apt-get update && apt-get upgrade -y
-apt-get dist-upgrade
+apt-get update
+#apt-get upgrade -y
+#apt-get dist-upgrade
 
 ###########################################
 ################## Apps ###################
 ###########################################
 apt-get install $PACKAGES -y
 
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+apt-get install nodejs
+node --version
+npm -v
+npm install -g @angular/cli
