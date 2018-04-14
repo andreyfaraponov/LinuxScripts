77772b3c52e43e949bbb17631e3e0711c2fac203
Andrey Faraponov <andrey.faraponov@gmail.com>
2018-04-14 12:06:59 +0300

    added display script

--- autoInstall.sh
+++ autoInstall.sh
@@ -4,7 +4,7 @@
 ###########################################
 HOSTNAME='fire'
 USERNAME='omega'
-PACKAGES='vim git build-essential curl chromium-browser libssl-dev apt-file'
+PACKAGES='xrandr vim git build-essential curl chromium-browser libssl-dev apt-file'
 
 ###########################################
 ################# Updates #################
@@ -16,8 +16,8 @@ apt-get update
 ###########################################
 ################## Apps ###################
 ###########################################
-apt-get install gconf-service libasound2 libatk1.0-0 libcairo2 libcups2 libfontconfig1 libgdk-pixbuf2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libxss1 fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils
 apt-get install $PACKAGES -y
+apt-get install gconf-service libasound2 libatk1.0-0 libcairo2 libcups2 libfontconfig1 libgdk-pixbuf2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libxss1 fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils
 
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 apt-get install nodejs
